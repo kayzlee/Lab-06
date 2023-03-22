022344058b8f189339ab2681285202bb66c97507
kayzlee <128451151+kayzlee@users.noreply.github.com>
2023-03-22 15:00:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-def encoder(password):
+encoder(password):
     encoder = ''
-    password = list(password)
-    for i in range(len(password)):
-        password[i] += 3
-        encoder += i
-        str(password)
+    password_str = str(password)
+    for i in range(len(password_str)):
+        encoder += str(int(password_str[i]) + 3)
     return encoder
 
 def main():
     is_valid_input = True
+    new_password = 0
     while is_valid_input:
         print('Menu\n-------------')
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
         option = int(input('\nPlease enter an option:'))
-        password = int(input('Please enter your password to encode:'))
+
+        if option == 1:
+            password = int(input('Please enter your password to encode:'))
+            new_password = encoder(password)
+            print('Your password has been encoded and stored!')
+        elif option == 2:
+            print('Your encoded password is ', new_password , ', the original password is ', password, '.')
+        elif option == 3:
+            break
 
 if __name__ == '__main__':
     main()
